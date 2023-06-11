d5df6db9760ca10d7e6e74e78d871516c1e72ef9
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-06-11 16:18:12 +0400

    Newshub

--- src/components/header/components/navigator/index.tsx
+++ src/components/header/components/navigator/index.tsx
@@ -1,5 +1,5 @@
-import DropDown from "./dropDown";
-import Search from "./search";
+import DropDown from "./components/dropDown";
+import Search from "./components/search";
 import { useContext } from "react";
 import { HeaderContext } from "../../context/HeaderContext";
 import searchClose from "../../../../assets/images/searchFieldClose.svg";
@@ -16,7 +16,7 @@ const Navigator = () => {
     <div>
       <div className="flex justify-between font-[Helvetica] h-12 xl:w-auto w-96 overflow-x-scroll overflow-y-hidden xl:p-0 px-4">
         <div>
-          <ul className="flex items-center justify-center space-x-6 font-[Helvica] text-[#475467] text-sm font-medium leading-4 cursor-pointer ">
+          <ul className="flex items-center justify-center space-x-8 font-[Helvica] text-[#475467] text-sm font-medium leading-4 cursor-pointer ">
             <li className="h-8 hover:border-b-2 hover:border-blue-400 hover:border-[#1989F0] hover:text-[#1989F0]">
               მთავარი
             </li>
@@ -57,9 +57,9 @@ const Navigator = () => {
           {!isOpenSearchField ? (
             <div
               onClick={() => toggleSearchField()}
-              className="flex justify-center h-7 font-[Helvetica] w-36 ml-14 border-l-2 border-[#EAECF0] cursor-pointer"
+              className="flex justify-center h-7 font-[Helvetica] w-36 border-l-2 border-[#EAECF0] cursor-pointer"
             >
-              <span className="material-symbols-outlined  ml-7 text-[#475467]">
+              <span className="material-symbols-outlined ml-14  text-[#475467]">
                 search
               </span>
               <span className="text-[#475467]">ძიება</span>
@@ -67,7 +67,7 @@ const Navigator = () => {
           ) : (
             <div
               onClick={() => toggleSearchField()}
-              className="flex justify-center h-7 w-36 font-[Helvetica] ml-14 border-l-2 border-[#EAECF0] cursor-pointer"
+              className="flex justify-center h-7 w-36 font-[Helvetica]  border-l-2 border-[#EAECF0] cursor-pointer"
             >
               <img className="ml-7" src={searchClose} />
             </div>
