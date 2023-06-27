0ddaf1c7f26d41fbc4fa1354acc252c546b24ec8
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-06-27 15:14:55 +0400

    Newshub

--- src/components/header/components/headerBottom/navigator/components/navigation/components/more/index.tsx
+++ src/components/header/components/headerBottom/navigator/components/navigation/components/more/index.tsx
@@ -2,20 +2,21 @@ import { useContext } from "react";
 import { HeaderContext } from "../../../../../../../context/HeaderContext";
 
 import DropDown from "../../../dropDown";
-import ArrowIcon from "../../../../../../../../../assets/images/moreComponentArrow.svg";
 
 const More = () => {
   const { toggleDropDown, isOpenDropDown } = useContext(HeaderContext);
 
   return (
-    <div className="relative text-lg font-medium not-italic font-case font-Helvetica">
+    <div className="relative">
       <div
         onClick={() => toggleDropDown()}
         className=" hover:text-[#1989F0] h-14 ml-7"
       >
         <div className="flex items-center">
-          <span>მეტი</span>
-          <img src={ArrowIcon} alt="arrow" className="w-3 ml-1 mt-1" />
+          <span className="text-lg font-medium not-italic font-case font-Helvetica">
+            მეტი
+          </span>
+          <span className="mt-1 material-symbols-outlined">expand_more</span>
         </div>
       </div>
       {isOpenDropDown ? <DropDown /> : null}
--- src/components/newsFeedSectionContainer/dayNews/components/currency/components/banks/index.tsx
+++ src/components/newsFeedSectionContainer/dayNews/components/currency/components/banks/index.tsx
@@ -12,19 +12,24 @@ const Banks = () => {
   } = useBank();
 
   return (
-    <div className="z-40 w-48 cursor-pointer m-4">
-      <div
-        className="flex items-center text-[#101828] font-medium order-1 not-italic font-case font-Helvetica bg-[#9FB4BC] rounded-xl p-2 justify-between"
-        onClick={handleDropdownToggle}
-      >
-        {selectedBank
-          ? banks.find((bank) => bank.id === selectedBank)?.label ||
-            banks[0].label
-          : "აირჩიე ბანკი"}
-        <img src={ArrowIcon} alt="arrow" className="w-3 ml-1 mt-1" />
+    <div
+      className="flex items-center  z-40 cursor-pointer m-4 relative"
+      onClick={handleDropdownToggle}
+    >
+      <div className="flex w-40 text-lg justify-between text-[#101828] bg-[#9FB4BC] rounded-xl p-2 ">
+        <div className="font-medium order-1 not-italic font-case font-Helvetica">
+          {selectedBank
+            ? banks.find((bank) => bank.id === selectedBank)?.label ||
+              banks[0].label
+            : "აირჩიე ბანკი"}
+        </div>
+        <div>
+          <span className="mt-1 material-symbols-outlined">expand_more</span>
+        </div>
       </div>
+
       {isDropdownOpen && (
-        <ul className="absolute w-48 text-sm border rounded p-2 bg-[#9FB4BC]">
+        <ul className="absolute top-12 w-48 text-sm border rounded p-2 bg-[#9FB4BC]">
           {dropdownLanguages.map((bank) => (
             <li
               key={bank.id}
