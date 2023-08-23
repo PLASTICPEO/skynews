f0c95654c7d77ecaf462a28ba69476cb082dd128
PLASTICPEO <giodavlasheridze88@gmail.com>
2023-08-23 18:14:49 +0400

    newshub

--- src/components/header/components/headerMiddle/components/logo/index.tsx
+++ src/components/header/components/headerMiddle/components/logo/index.tsx
@@ -1,6 +1,6 @@
 import { HeaderContext } from "../../../../context/HeaderContext";
 import { useContext } from "react";
-import heroLogo from "../../../../../../assets/images/NEWSHUBlogo.svg";
+import heroLogo from "../../../../../../assets/images/NEWSHUB.svg";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
@@ -9,12 +9,14 @@ const Logo = () => {
   return (
     <Link to="/">
       <div
-        className={`flex ${prevScrollPos > 50 ? "mb-0" : "mb-8"} xl:ml-0 ml-4`}
+        className={`flex xl:mt-2 mt-10 ${
+          prevScrollPos > 50 ? "mb-0" : "mb-11"
+        } xl:ml-0 ml-4`}
       >
         <img
           className={`${
-            prevScrollPos > 50 ? "w-24" : "w-32"
-          }  cursor-pointer m-auto`}
+            prevScrollPos > 50 ? "xl:w-36" : "xl:w-96"
+          } w-60 cursor-pointer m-auto`}
           src={heroLogo}
           alt="logo"
           onClick={scrollPositionTop}
